7749f420fb273857401112592bade06082b58df8
sri812 <srilekha812@gmail.com>
2021-12-09 15:11:15 +0530

    Initial Commit

---
+++ src/main/java/com/service/financialanalyzer/FinancialanalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.service.financialanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialanalyzerApplication.class, args);
+	}
+
+}
